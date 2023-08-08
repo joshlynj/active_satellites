58835eadbfc1c1fbedd2196624a1fdf62094c883
joshlynj <joshlyn294@gmail.com>
2023-08-08 11:01:47 -0600

    added regression function

--- src/clean.py
+++ src/clean.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import statsmodels.api as sm
 
 sat_df = pd.read_csv('/Users/joshy/Desktop/dai/daimil5/projects/final/'
                      'active_satellites/data/database.csv')
@@ -48,9 +50,22 @@ def get_dummies(sat_df):
     return orbit_df
 
 
+def make_reg(X, y):
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    X_scaled = sm.add_constant(X_scaled)
+
+    model = sm.OLS(y, X_scaled).fit()
+    return model
+
+
 if __name__ == '__main__':
     print(drop_na(sat_df))
     print(sat_df['Expected Lifetime (Years)'].apply(clean_lifetime))
     print(make_numeric(sat_df))
     print(sat_df.apply(remove_space))
     print(get_dummies(sat_df))
+    print(make_reg(sat_df[['Perigee (Kilometers)',
+                           'Inclination (Degrees)']],
+                   sat_df['Apogee (Kilometers)']))
