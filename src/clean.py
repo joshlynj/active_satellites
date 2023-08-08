ac0e8f9df83432a99b9bcd2c7bdd85b4250285bd
joshlynj <joshlyn294@gmail.com>
2023-08-08 09:55:24 -0600

    added two more cleaning functions

--- src/clean.py
+++ src/clean.py
@@ -15,7 +15,7 @@ def drop_na(sat_df):
 def clean_lifetime(value):
     ''' Function takes in a value
         Function replaces strings and takes mean value of ranges
-        Returns a cleaned float value'''
+        Returns a cleaned float value '''
     value = value.replace(' yr.', '') \
                  .replace(' hrs.', '') \
                  .replace(' trs', '') \
@@ -27,6 +27,22 @@ def clean_lifetime(value):
     return float(value)
 
 
+def make_numeric(sat_df):
+    ''' Function takes in a dataframe
+        Returns a dataframe with the launch mass column as an int '''
+    pd.to_numeric(sat_df["Launch Mass (Kilograms)"],
+                  errors='coerce')
+    return sat_df
+
+
+def remove_space(value):
+    ''' Function takes in a value
+        Returns a value with the space removed '''
+    return value.replace('LEO ', 'LEO')
+
+
 if __name__ == '__main__':
     print(drop_na(sat_df))
     print(sat_df['Expected Lifetime (Years)'].apply(clean_lifetime))
+    print(make_numeric(sat_df))
+    print(sat_df.apply(remove_space))
